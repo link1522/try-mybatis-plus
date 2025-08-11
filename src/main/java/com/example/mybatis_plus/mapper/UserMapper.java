103380266e8c54ed8f02020538ff6719e5d211dd
Link1515 <h26281660@gmail.com>
2025-08-11 10:24:53 +0800

    add User entity and test it

---
+++ src/main/java/com/example/mybatis_plus/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.example.mybatis_plus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.mybatis_plus.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+    
+}
--- src/test/java/com/example/mybatis_plus/MybatisPlusApplicationTests.java
+++ src/test/java/com/example/mybatis_plus/MybatisPlusApplicationTests.java
@@ -1,13 +1,22 @@
 package com.example.mybatis_plus;
 
+import java.util.List;
+
+import javax.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.mybatis_plus.entity.User;
+import com.example.mybatis_plus.mapper.UserMapper;
+
 @SpringBootTest
 class MybatisPlusApplicationTests {
+	@Resource
+	private UserMapper userMapper;
 
 	@Test
-	void contextLoads() {
+	void testSelectList() {
+		List<User> users = userMapper.selectList(null);
+		users.forEach(System.out::println);
 	}
-
 }
