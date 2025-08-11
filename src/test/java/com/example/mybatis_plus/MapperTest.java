ca5b5cebf49fd8dfe236dd42a27f0773f2cbfdba
Link1515 <h26281660@gmail.com>
2025-08-11 11:05:42 +0800

    test mapper common methods

---
+++ src/test/java/com/example/mybatis_plus/MapperTest.java
@@ -0,0 +1,59 @@
+package com.example.mybatis_plus;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.mybatis_plus.entity.User;
+import com.example.mybatis_plus.mapper.UserMapper;
+
+@SpringBootTest
+public class MapperTest {
+    @Resource
+    private UserMapper userMapper;
+
+    @Test
+    void testInsert() {
+        User user = new User();
+        user.setName("Mandy");
+        user.setAge(22);
+        user.setEmail("test6@test.com");
+        int result = userMapper.insert(user);
+        System.out.println(result);
+    }
+
+    @Test
+    void testSelect() {
+        User user = userMapper.selectById(1);
+        System.out.println(user);
+
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        users.forEach(System.out::println);
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("name", "Mandy");
+        map.put("age", 22);
+        List<User> users1 = userMapper.selectByMap(map);
+        users1.forEach(System.out::println);
+    }
+
+    @Test
+    void testUpdate() {
+        User user = new User();
+        user.setId(1L);
+        user.setAge(36);
+        int result = userMapper.updateById(user);
+        System.out.println(result);
+    }
+
+    @Test
+    void testDelete() {
+        int result = userMapper.deleteById(1);
+        System.out.println(result);
+    }
+}
