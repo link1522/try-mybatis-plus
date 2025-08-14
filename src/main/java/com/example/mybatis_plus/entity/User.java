b94d4fbd6a22e51df47cb8a16bbf62c8853ba7f4
Link1515 <h26281660@gmail.com>
2025-08-14 15:23:21 +0800

    modify table name from entity

--- src/main/java/com/example/mybatis_plus/entity/User.java
+++ src/main/java/com/example/mybatis_plus/entity/User.java
@@ -1,8 +1,11 @@
 package com.example.mybatis_plus.entity;
 
+// import com.baomidou.mybatisplus.annotation.TableName;
+
 import lombok.Data;
 
 @Data
+// @TableName(value = "t_user")
 public class User {
     private Long id;
     private String name;
