a4ecf7151e8612412b637cf4bb55f6a782d22e31
Link1515 <h26281660@gmail.com>
2025-08-17 09:53:09 +0800

    field auto fill

--- src/main/java/com/example/mybatis_plus/entity/User.java
+++ src/main/java/com/example/mybatis_plus/entity/User.java
@@ -1,7 +1,9 @@
 package com.example.mybatis_plus.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 
 // import com.baomidou.mybatisplus.annotation.TableName;
 
@@ -11,9 +13,17 @@ import lombok.Data;
 // @TableName(value = "t_user")
 public class User {
     // 設定主鍵策略 IdType.ASSIGN_ID => 預設值 (雪花算法)
-    @TableId(type=IdType.ASSIGN_ID)
+    // @TableId(type=IdType.ASSIGN_ID)
     private Long id;
+
+    // 直接設定資料庫中的欄位名稱
+    // @TableField(value = "username")
     private String name;
     private Integer age;
     private String email;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
 }
---
+++ src/main/java/com/example/mybatis_plus/handler/MyMetaObjectHandler.java
@@ -0,0 +1,42 @@
+package com.example.mybatis_plus.handler;
+
+import java.time.LocalDateTime;
+
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("insert 填充 datetime");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+
+        // 只有在欄位沒有給值時才執行
+        Object age = this.getFieldValByName("age", metaObject);
+        if (age == null) {
+            this.strictInsertFill(metaObject, "age", Integer.class, 16);
+        }
+
+        // 只針對有特定欄位的表
+        boolean hasAuthor = metaObject.hasSetter("author");
+        if (hasAuthor) {
+            this.strictInsertFill(metaObject, "author", String.class, "default");
+        }
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("update 填充 datetime");
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        // this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+    
+}
